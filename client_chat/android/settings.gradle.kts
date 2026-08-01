f519f12915d9b3e0ddafb64442ed0e3a085d6b63
techinfraconnect <zudiocharm@gmail.com>
2026-08-01 21:53:35 +0530

    Fixes - try

--- client_chat/android/settings.gradle.kts
+++ client_chat/android/settings.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false // Use 1.9.24 to keep compatibility with older plugins like webrtc
 }
 
 include(":app")
