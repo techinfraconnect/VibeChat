bfc6d81361f7b018a6fef3ff23d2fde992c6c855
techinfraconnect <zudiocharm@gmail.com>
2026-08-03 13:47:35 +0530

    Jitsi SDK - moving to Jitsi SDK

--- client_chat/windows/flutter/generated_plugins.cmake
+++ client_chat/windows/flutter/generated_plugins.cmake
@@ -3,8 +3,16 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  audioplayers_windows
+  connectivity_plus
   firebase_core
-  flutter_webrtc
+  flutter_volume_controller
+  permission_handler_windows
+  screen_brightness_windows
+  share_plus
+  url_launcher_windows
+  zego_express_engine
+  zego_zim
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
